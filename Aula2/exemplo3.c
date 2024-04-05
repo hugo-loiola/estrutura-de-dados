6195d5d6ce5e3dd5df606173d5db555c8d9c419c
Hugo Loiola <hugoloiola.1@gmail.com>
2024-04-05 11:18:56 -0300

    Teste

--- Aula2/exemplo3.c
+++ Aula2/exemplo3.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <math.h>
+#include <string.h>
 
 struct Pessoa
 {
@@ -9,13 +10,24 @@ struct Pessoa
   float Peso;
 } typedef Pessoa;
 
-int main()
+Pessoa CadastrarPessoa();
+void ExibirPessoa(Pessoa X);
+float CalcularIMC(Pessoa X);
+void FazendoAniversario(Pessoa *X);
+
+void main()
 {
   Pessoa X = CadastrarPessoa();
 
   ExibirPessoa(X);
 
-    return 0;
+  FazendoAniversario(&X);
+
+  ExibirPessoa(X);
+
+  printf("O IMC de e igual a %.3f", CalcularIMC(X));
+
+  return 0;
 }
 
 Pessoa CadastrarPessoa()
@@ -23,7 +35,7 @@ Pessoa CadastrarPessoa()
 
   Pessoa P;
   printf("Nome: ");
-  scanf("%s", P.Nome);
+  gets(P.Nome);
 
   printf("Idade: ");
   scanf("%d", &P.Idade);
@@ -33,14 +45,22 @@ Pessoa CadastrarPessoa()
 
   printf("Peso: ");
   scanf("%f", &P.Peso);
-};
+}
 
 void ExibirPessoa(Pessoa X)
 {
   printf("%s %d %.2f %.2f\n", X.Nome, X.Idade, X.Altura, X.Peso);
-};
+}
 
 float CalcularIMC(Pessoa X)
 {
   float IMC = X.Peso / pow(X.Altura, 2);
+  return (IMC);
+}
+
+void FazendoAniversario(Pessoa *X)
+{
+  X->Idade = X->Idade + 1;
+
+  // Usando -> no lugar de . pois X é um ponteiro da struct
 }
