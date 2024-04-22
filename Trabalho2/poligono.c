7ebb27e24a069a96faef56b7ac7b2e206d4a5fe3
Hugo Loiola <hugoloiola.1@gmail.com>
2024-04-22 13:38:36 -0300

    Inicio trabalho 2

---
+++ Trabalho2/poligono.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct
+{
+  float X;
+  float Y;
+} Ponto;
+
+float AreaTriangulo(Ponto A, Ponto B, Ponto C){
+
+};
+
+int main()
+{
+
+  return 0;
+}
